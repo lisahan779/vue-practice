402f36e059a19c49e8c782e41accd6731989a1f3
韩敏敏 <7344084+han_min_min@user.noreply.gitee.com>
2020-10-21 17:21:29 +0800

    本地

--- README.md
+++
@@ -1,4 +0,0 @@
-# vue-practi
-
-  node-vue-moba全栈项目 包含三个（admin 后台管理系统  web前端展示  server 服务端） 线上项目
-  node-vue-moba1 本地项目
--- node-vue-moba/server/models/Item.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const schema = new mongoose.Schema({
-  name: { type: String },
-  icon: { type: String }, //保存图片地址
-})
-
-module.exports = mongoose.model('Item', schema)
